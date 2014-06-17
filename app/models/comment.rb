ff9677277b7584a370777ab91e8d6b01973d32af
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-17 16:00:07 -0700

    add fragementation caching

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   attr_accessible :body, :post
-  belongs_to :post
+  belongs_to :post, touch: true
+  # belongs_to :post
   has_many :replies, dependent: :destroy
 end
